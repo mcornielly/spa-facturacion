09cda83f47f2e5bf52c29e2eec67529962b3bc97
mcorielly <mcorielly@gmail.com>
2019-03-14 00:55:22 -0400

    Completed InvoiceController

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -43,4 +43,117 @@ class InvoiceController extends Controller
 
     	return [compact('form')];
     }
+
+    public function store(Request $request)
+    {
+    	$this->validate([
+    		'customer_id' 	=> 'required|integer|exists:customers, id',
+    		'date' 			=> 'required|date_format:Y-m-d',
+    		'due_date' 		=> 'required|date_format:Y-m-d',
+    		'reference'		=> 'nullable|max:100',
+    		'discount' 		=> 'required|numeric|min:0',
+    		'terms_and_conditions'	=> 'required|max:2000',
+    		'items'			=> 'required|array|min:1',
+    		'items.*.product_id' => 'required|integer|exists:products, id',
+    		'items.*.unit_price' => 'required|numeric|min:0',
+    		'items.*.qty'		 => 'required|integer|min:1'					
+    	]);
+
+    	$invoice = new Invoice;
+    	$invoice->fill($request->except('items'));
+
+
+    	$invoice->sub_total = collect($request->items)->sum(function($item){
+    		return $item['qty'] * $item['unit_price'];
+    	});
+
+    	$invoice = DB::transaction(function() use($invoice, $request){
+    		$counter = Counter::where('key', 'invoice')->first();
+
+    		$invoice->number = $counter->prefix . $counter->value;
+	
+	    	
+	    	//custom method from app/Helper/HasManyRelation
+	    	$invoice->storeHasMany([
+	    		'items' => $request->items
+	    	]);
+
+	    	$counter->increment('value');
+
+	    	return $invoice;
+
+    	});
+
+    	return response()
+    		->json(['saved' => true, 'id' => $invoice->id]);
+    }
+
+    public function show($id)
+    {
+    	$invoice = Invoice::with(['customer', 'items.product'])
+    		->findOrFail($id);
+
+    	return [compact('invoice')];	
+    }
+
+    public function edit($id)
+    {
+    	$invoice = Invoice::with(['customer', 'items.product'])
+    		->findOrFail($id);
+
+    	return [compact('invoice')];	
+    }
+
+    public function update(Request $request, $id)
+    {
+    	$invoice = Invoice::findOrFail($id);
+
+    	$this->validate([
+    		'customer_id' 	=> 'required|integer|exists:customers, id',
+    		'date' 			=> 'required|date_format:Y-m-d',
+    		'due_date' 		=> 'required|date_format:Y-m-d',
+    		'reference'		=> 'nullable|max:100',
+    		'discount' 		=> 'required|numeric|min:0',
+    		'terms_and_conditions'	=> 'required|max:2000',
+    		'items'			=> 'required|array|min:1',
+    		'items.*.id'	=> 'sometimes|required|integer|exists:invoce_items,id,invoice_id'.$invoice->id,
+    		'items.*.product_id' => 'required|integer|exists:products, id',
+    		'items.*.unit_price' => 'required|numeric|min:0',
+    		'items.*.qty'		 => 'required|integer|min:1'					
+    	]);
+
+    	$invoice->fill($request->except('items'));
+
+    	$invoice->sub_total = collect($request->items)->sum(function($item){
+    		return $item['qty'] * $item['unit_price'];
+    	});
+
+    	$invoice = DB::transaction(function() use($invoice, $request){
+	    	
+	    	//custom method from app/Helper/HasManyRelation
+	    	$invoice->updateHasMany([
+	    		'items' => $request->items
+	    	]);
+
+	    	return $invoice;
+
+    	});
+
+    	return response()
+    		->json(['saved' => true, 'id' => $invoice->id]);
+    }
+
+    public function destroy($id)
+    {
+    	$invoice = Invoice::findOrFail($id);
+
+    	$invoice->items()->delete();
+    	$invoice->delete();
+
+    	// return responce()
+    	// 	->json(['deleted' => true]);	
+    	return [compact('invoice')];
+    }
+
+
 }
